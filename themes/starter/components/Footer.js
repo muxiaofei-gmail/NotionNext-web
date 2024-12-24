7f9d21997a7c128ed4330f643008168135d290ca
muxiaofei-gmail <jiaguodong123@gmail.com>
2024-12-24 17:27:54 +0800

    Update Footer.js
    更改下方博文界面

--- themes/starter/components/Footer.js
+++ themes/starter/components/Footer.js
@@ -64,32 +64,24 @@ export const Footer = props => {
 
             {/* 页脚右侧最新博文 */}
             <div className='w-full px-4 md:w-2/3 lg:w-6/12 xl:w-3/12'>
-              <div className='mb-10 w-full'>
-                <h4 className='mb-9 text-lg font-semibold text-white'>
+              <div className='mb-10 w-full bg-white p-2 flex flex-col items-center rounded-lg hover:scale-110 ease-in-out'>
+                <h4 className='mb-3 text-lg font-semibold text-black mt-4'>
                   {siteConfig('STARTER_FOOTER_BLOG_LATEST_TITLE')}
                 </h4>
+                <p className='text-sm text-black text-center mb-3'>
+                  扫描二维码添加客服微信在线咨询。
+                </p>
                 {/* 展示两条最新博客文章 */}
-                <div className='flex flex-col gap-8'>
-                  {latestPosts?.map((item, index) => {
-                    return (
-                      <a
-                        key={index}
-                        href={item?.href}
-                        className='group flex items-center gap-[22px]'>
-                        {item.pageCoverThumbnail && (
-                          <div className='overflow-hidden rounded w-20 h-12'>
-                            <img
-                              src={item.pageCoverThumbnail}
-                              alt={item.title}
-                            />
-                          </div>
-                        )}
-                        <span className='line-clamp-2 max-w-[180px] text-base text-gray-7 group-hover:text-white'>
-                          {item.title}
-                        </span>
-                      </a>
-                    )
-                  })}
+                <div className='flex flex-col gap-8 items-center'>
+                  <div
+                    className="mb-4 sm:mb-8 h-[100px] w-[100px] flex justify-center items-center"
+                  >
+                    <img
+                      src={siteConfig('STARTER_WX', null, CONFIG)}
+                      alt="about image"
+                      className="h-[100px] w-[100px] object-cover object-center"
+                    />
+                  </div>
                 </div>
               </div>
             </div>

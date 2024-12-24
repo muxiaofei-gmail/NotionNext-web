25a3b7cc225351c19dcee493db9a39752e955158
muxiaofei-gmail <jiaguodong123@gmail.com>
2024-12-24 17:03:46 +0800

    Update Header.js
    py-2-------px-6 py-3

--- themes/starter/components/Header.js
+++ themes/starter/components/Header.js
@@ -96,12 +96,12 @@ export const Header = props => {
                   <div className='hidden sm:flex gap-4'>
                     <a
                       href={siteConfig('STARTER_NAV_BUTTON_1_URL')}
-                      className={`loginBtn ${buttonTextColor} p-2 text-base font-medium hover:opacity-70`}>
+                      className={`loginBtn ${buttonTextColor} p-2 text-base font-medium hover:opacity-70 hidden`}>
                       {siteConfig('STARTER_NAV_BUTTON_1_TEXT')}
                     </a>
                     <a
                       href={siteConfig('STARTER_NAV_BUTTON_2_URL')}
-                      className={`signUpBtn ${buttonTextColor} p-2 rounded-md bg-white bg-opacity-20 py-2 text-base font-medium duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark`}>
+                      className={`signUpBtn ${buttonTextColor} p-2 rounded-md bg-white bg-opacity-20 px-6 py-3 text-base font-medium duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark`}>
                       {siteConfig('STARTER_NAV_BUTTON_2_TEXT')}
                     </a>
                   </div>

8dd54fc7c5292f856058963eab04fcb9b32d908d
muxiaofei-gmail <jiaguodong123@gmail.com>
2024-12-24 15:13:21 +0800

    Update Header.js
    bg-transparent

--- themes/starter/components/Header.js
+++ themes/starter/components/Header.js
@@ -55,7 +55,7 @@ export const Header = props => {
   return (
     <>
       {/* <!-- ====== Navbar Section Start --> */}
-      <div className='ud-header absolute left-0 top-0 z-40 flex w-full items-center bg-[#FFFFFF]'>
+      <div className='ud-header absolute left-0 top-0 z-40 flex w-full items-center bg-transparent'>
         <div className='container'>
           <div className='relative -mx-4 flex items-center justify-between'>
             {/* Logo */}
